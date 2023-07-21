923b05462c982b7bd6a781278710b14a4b3731c9
Xiongxiao Xu <34889516+XiongxiaoXu@users.noreply.github.com>
2023-07-21 10:16:50 -0500

    Add files via upload

---
+++ algorithm.py
@@ -0,0 +1,92 @@
+from statsmodels.tsa.arima.model import ARIMA
+import pandas as pd
+import inspect
+import torch
+import torch.nn as nn
+from utilis import *
+from model import LSTM
+
+def myARIMA(train, test, args):
+    args.function_name = inspect.stack()[0][3]
+    print(args.function_name)
+    window = train[-args.backward_window_size:].tolist()
+    predict = []
+    real = window
+
+    for i in range(len(test)):
+        model = ARIMA(window, order=(13,1,5))
+        model_fit = model.fit()
+        predict = predict + model_fit.forecast(args.forward_window_size).tolist()
+        real.append(test.iloc[i])
+        
+        if args.num_lagstep == 1:
+            window = real[-args.backward_window_size:]
+        else:
+            if len(predict) < args.num_lagstep-1:
+                window = real[-args.backward_window_size:-len(predict)] + predict[-len(predict):]
+            else:
+                window = real[-args.backward_window_size:-(args.num_lagstep-1)] + predict[-(args.num_lagstep-1):]
+
+    predict = pd.Series(predict, index=test.index)
+
+    return predict
+
+def ADPLSTM(train, test, args):
+    args.function_name = inspect.stack()[0][3]
+    print(args.function_name)
+    train_inout_seq = create_inout_sequences(torch.FloatTensor(train), args.backward_window_size, args.forward_window_size)
+    input_size = 1
+    lstm = LSTM(input_size, args.hidden_dim, args.forward_window_size)
+    optimizer = torch.optim.Adam(lstm.parameters(), lr=0.001)
+    loss_function = nn.MSELoss()
+    lstm.train()
+
+    args.epoch = 50
+    for i in range(args.epoch):
+        epoch_loss = 0
+        for seq, label in train_inout_seq:
+            seq = seq.reshape(1, -1, 1)
+            optimizer.zero_grad()
+            y_pred = lstm(seq)
+            loss = loss_function(y_pred, label)
+            loss.backward()
+            optimizer.step()
+            epoch_loss = epoch_loss + loss
+
+    lstm.eval()
+
+    window = train[-args.backward_window_size:].tolist()
+    predict = []
+    real = window
+
+    for i in range(len(test)):
+        seq = torch.FloatTensor(window).reshape(1, -1, 1)
+        with torch.no_grad():
+            predict = predict + [float(torch.squeeze(lstm(seq)))]
+        real = real + test.iloc[i*args.forward_window_size:(i+1)*args.forward_window_size].tolist()
+        if args.num_lagstep == 1:
+            window = real[-args.backward_window_size:]
+        else:
+            if len(predict) < args.num_lagstep-1:
+                window = real[-args.backward_window_size:-len(predict)] + predict[-len(predict):]
+            else:
+                window = real[-args.backward_window_size:-(args.num_lagstep-1)] + predict[-(args.num_lagstep-1):]
+
+        args.online_training_epoch = 1
+        train_inout_seq = create_inout_sequences(torch.FloatTensor(real[-(args.backward_window_size + args.forward_window_size):]), 
+                                                    args.backward_window_size, args.forward_window_size)
+        lstm.train()
+        for i in range(args.online_training_epoch):
+            for seq, label in train_inout_seq:
+                seq = seq.reshape(1, -1, 1)
+                optimizer.zero_grad()
+                y_pred = lstm(seq)
+                loss = loss_function(y_pred, label)
+                loss.backward()
+                optimizer.step()
+        lstm.eval()
+
+    predict = pd.Series(predict[-len(test):], index=test.index)
+
+    return predict
+
---
+++ utilis.py
@@ -0,0 +1,67 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+import os
+
+def load_traffic_data(args):
+    df = pd.read_csv('Data/' + args.placement + '/' + args.RIDPID + '.csv')
+    PID = args.RIDPID[-2:]
+    dic_PIDtoFullName = {'L0': 'Local0', 'L1': 'Local1', 'L2': 'Local2', 'G0': 'Global0', 'G1': 'Global1', 'T0': 'Terminal0', 'T1': 'Terminal1'}
+    data = df[dic_PIDtoFullName[PID]]
+    data = normalize_minmax(data)
+    train = data[0:args.num_train]
+
+    # Stop data after 1380/1430 timepoint because the system enters unsteady state
+    if args.placement == 'cont-adp':
+        test = data[args.num_train:args.num_train + 1380]
+    elif args.placement == 'rand_node-adp':
+        test = data[args.num_train:args.num_train + 1430]
+
+    return train, test
+
+def normalize_minmax(data):
+    scaler = MinMaxScaler()
+    data = scaler.fit_transform(data.values.reshape(-1,1))
+    data = pd.Series(np.squeeze(data), index=range(len(data)))
+
+    return data
+
+def evaluate(y, pred_y):
+    MSE = np.mean((y - pred_y) ** 2)
+    MAE = np.mean(np.abs(y - pred_y))
+
+    return MSE, MAE
+
+def create_inout_sequences(input_data, backward_window_size, forward_window_size):
+    inout_seq = []
+    L = len(input_data)
+    for i in range(L - backward_window_size - forward_window_size + 1):
+        train_seq = input_data[i:i + backward_window_size]
+        train_label = input_data[i + backward_window_size:i + backward_window_size + forward_window_size]
+        inout_seq.append((train_seq ,train_label))
+
+    return inout_seq
+
+def plot(train, test, predict, args):
+    plt.figure(figsize = (12,5), dpi = 100)
+    size = 15
+    font_label = {'family': 'Verdana', 'weight': 'normal', 'size': size}
+    font_legend = {'family': 'Verdana', 'weight': 'normal', 'size': size}
+
+    plt.ylim(-0.1, 1.3)
+    plt.plot(train, label='Train')
+    plt.plot(test, label='Test')
+    plt.plot(predict, label='Forecast')
+
+    plt.xlabel('Time', fontdict=font_label)
+    plt.ylabel('Traffic', fontdict=font_label)
+    plt.tick_params(axis='x', labelsize=size)
+    plt.tick_params(axis='y', labelsize=size)
+
+    plt.legend(prop=font_legend, loc='best', mode="expand", ncol=3)
+    folder_path = 'Figure/' + args.placement + '/' + args.RIDPID
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    path = folder_path + '/' + args.RIDPID + '_' + args.function_name + '.png'
+    plt.savefig(path)
